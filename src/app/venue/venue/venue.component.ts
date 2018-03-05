a7f75123743b269e8e59084a67d5e0e210a8f360
mmatczak <marek.matczak@capgemini.com>
2018-03-05 14:21:54 +0100

    Venue look and feel improved

--- src/app/venue/venue/venue.component.ts
+++ src/app/venue/venue/venue.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'ea-venue',
@@ -6,11 +7,26 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./venue.component.scss']
 })
 export class VenueComponent implements OnInit {
-  title = 'My first AGM project';
   lat = 51.1086552;
   lng = 17.0391495;
+  googleMapsUrl;
 
-  constructor() {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    const protocol = this.getProtocol();
+    this.googleMapsUrl =
+      `${protocol}://www.google.com/maps/search/?api=1&query=${this.lat},${this.lng}&query_place_id=ChIJ4f2vj3fCD0cRM05-5W5lV5Y`;
+  }
+
+  private getProtocol() {
+    let protocol = 'https';
+    if (isPlatformBrowser(this.platformId)) {
+      if ((navigator.platform.indexOf('iPhone') !== -1)
+        || (navigator.platform.indexOf('iPod') !== -1)
+        || (navigator.platform.indexOf('iPad') !== -1)) {
+        protocol = 'maps';
+      }
+    }
+    return protocol;
   }
 
   ngOnInit() {
