084db3e7f3c2447a82624895f8b24ad8f16da2ad
mmatczak <marek.matczak@capgemini.com>
2018-03-05 15:13:05 +0100

    Directions improved on iOS

--- src/app/venue/venue/venue.component.ts
+++ src/app/venue/venue/venue.component.ts
@@ -1,35 +1,14 @@
-import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
-import {isPlatformBrowser} from '@angular/common';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ea-venue',
   templateUrl: './venue.component.html',
   styleUrls: ['./venue.component.scss']
 })
-export class VenueComponent implements OnInit {
+export class VenueComponent {
   lat = 51.1086552;
   lng = 17.0391495;
-  googleMapsUrl;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    const protocol = this.getProtocol();
-    this.googleMapsUrl =
-      `${protocol}://www.google.com/maps/search/?api=1&query=${this.lat},${this.lng}&query_place_id=ChIJ4f2vj3fCD0cRM05-5W5lV5Y`;
+  constructor() {
   }
-
-  private getProtocol() {
-    let protocol = 'https';
-    if (isPlatformBrowser(this.platformId)) {
-      if ((navigator.platform.indexOf('iPhone') !== -1)
-        || (navigator.platform.indexOf('iPod') !== -1)
-        || (navigator.platform.indexOf('iPad') !== -1)) {
-        protocol = 'maps';
-      }
-    }
-    return protocol;
-  }
-
-  ngOnInit() {
-  }
-
 }
