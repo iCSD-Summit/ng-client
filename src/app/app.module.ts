b3926236cfecabc9808520375319f004edb78146
mmatczak <marek.matczak@capgemini.com>
2018-03-08 12:49:13 +0100

    Service Worker added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {CoreModule} from './core/core.module';
 import {AgendaModule} from './agenda/agenda.module';
 import {SharedModule} from './shared/shared.module';
 import {VenueModule} from './venue/venue.module';
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import {VenueModule} from './venue/venue.module';
   ],
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     CoreModule,
     SharedModule,
     AgendaModule,
