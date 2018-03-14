d2eab3df9b0f3244cfcf0379b54ede5d606bcda7
M.Suska <msuska@gmail.com>
2018-03-14 11:54:50 +0100

    added push notification to sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
 import {AgendaModule} from './agenda/agenda.module';
 import {SharedModule} from './shared/shared.module';
 import {VenueModule} from './venue/venue.module';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import { environment } from '../environments/environment.prod';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
     BrowserModule,
     CoreModule,
     SharedModule,
