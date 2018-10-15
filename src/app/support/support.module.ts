1b39b63268aedbc34393707b2caf463204568d43
Velando Cruz <javier.velando-cruz@capgemini.com>
2018-10-15 10:54:53 +0200

    added whatsapp icon to mat-icon

--- src/app/support/support.module.ts
+++ src/app/support/support.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SupportComponent } from './support/support.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [SupportComponent]
+  imports: [CommonModule, MatIconModule],
+  declarations: [SupportComponent],
 })
-export class SupportModule { }
+export class SupportModule {}
--- src/app/support/support/support.component.ts
+++ src/app/support/support/support.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'ea-support',
   templateUrl: './support.component.html',
-  styleUrls: ['./support.component.scss']
+  styleUrls: ['./support.component.scss'],
 })
 export class SupportComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      `icon_whatsapp`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        `../assets/whatsapp.svg`,
+      ),
+    );
   }
 
+  ngOnInit() {}
 }
