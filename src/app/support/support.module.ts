b361332f68cd1147a267e88960155658fa65a97b
Velando Cruz <javier.velando-cruz@capgemini.com>
2018-10-16 08:04:05 +0200

    Revert "Merge branch 'master' of https://github.com/iCSD-Summit/ng-client"
    This reverts commit 1eaa10b0cd9d82d4642f136bb018b4e7d358713e, reversing
    changes made to dddfcb229a5ba66a6cb57385c29b2d1ee773c23e.

--- src/app/support/support.module.ts
+++ src/app/support/support.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SupportComponent } from './support/support.component';
-import { SharedModule } from '../shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, MatIconModule],
   declarations: [SupportComponent],
 })
 export class SupportModule {}
